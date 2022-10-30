8fb35750fbdc51d924a48b2cffcff581f32adbdf
Vlad Zlatopolsky <vladislav.zlatopolsky@gmail.com>
2022-10-30 19:36:05 +0200

    adds cw#12 1 part

---
+++ html-css-js-introduction-dogram/app.js
@@ -0,0 +1,14 @@
+// ---------------------------
+console.log('---------------------------CW#12---------------------------');
+// ---------------------------
+let strNum1 = '123';
+let strNum2 = '9';
+// conversions from string to number
+console.log(strNum1 + strNum2); // => 1239
+console.log(strNum1 - strNum2); // => 114
+console.log(strNum1 > strNum2); // => false
+
+let num1 = +strNum1;
+let num2 = +strNum2;
+let strNumStr = '123ab';
+let numStr = strNumStr;
